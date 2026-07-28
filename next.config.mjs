31387abc139c45034effd1903b5944288de4cfd8
ryoshinkobayashi1020-pixel <ryoshinkobayashi1020-pixel@users.noreply.github.com>
2026-07-28 23:13:48 +0900

    Trace only Linux sharp binaries for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
   // otherwise omit libvips' Linux shared libraries from the serverless bundle.
   outputFileTracingIncludes: {
     "/api/accounting/invoice": [
-      "./node_modules/sharp/**/*",
-      "./node_modules/@img/**/*",
+      "./node_modules/.pnpm/@img+sharp-linux-x64@*/node_modules/@img/sharp-linux-x64/**/*",
+      "./node_modules/.pnpm/@img+sharp-libvips-linux-x64@*/node_modules/@img/sharp-libvips-linux-x64/**/*",
       "./assets/invoice-template.png",
       "./assets/fonts/**/*",
     ],
